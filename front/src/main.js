c3f652c142a4bd8199c491935c8a475047d1ac1d
filipzupancic <filipzupancic123@gmail.com>
2021-05-22 21:14:22 +0200

    Adding frontent functionalities

--- front/src/main.js
+++ front/src/main.js
@@ -1,8 +1,29 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+import Home from './components/Home.vue'
+import Diary from './components/Diary.vue'
+
+Vue.use(VueRouter)
 
 Vue.config.productionTip = false
 
+const router = new VueRouter({
+  mode: 'history',
+  routes: [
+    {
+      path: '/',
+      component: Home
+    },
+    {
+      path: '/diary',
+      component: Diary
+    }
+  ]
+
+})
+
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
