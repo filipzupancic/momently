94660583374d0a6c6b1b814d92aeff5d397f79a2
Aljaz V <aljaz.verlic@gmail.com>
2021-05-23 03:40:59 +0200

    changes

--- front/src/components/Diary.vue
+++ front/src/components/Diary.vue
@@ -9,7 +9,7 @@
             <hr style="background-color: #d8ac87; border-top: 1px; padding:0.5px; width: 10%;">
         </div>
         <div style=" padding-bottom: 30px;">
-          <HotelDatePicker />
+          <HotelDatePicker @period-selected='inputUpdated' />
         </div>
         <div class="col-xs-10 col-xs-offset-1 col-sm-8 col-sm-offset-2" style="margin-top: 30px;">
             <ul v-if="results && results.length" class="timeline timeline-centered">
@@ -106,6 +106,7 @@ import 'vue-hotel-datepicker/dist/vueHotelDatepicker.css';
 import Vue from 'vue'
 import Bars from 'vuebars'
 
+
 Vue.use(Bars)
 
 
@@ -132,6 +133,25 @@ export default {
 
       console.log(this.results)
   },
+  methods: {
+    inputUpdated: (event, dateFrom, dateTo) => {
+      console.log(event, dateFrom, dateTo);
+
+      axios.get('http://127.0.0.1:8000/events',  
+                {params: {
+                  dateFrom: dateFrom.toISOString().split('T')[0],
+                  dateTo: dateTo.toISOString().split('T')[0]
+                }}, {
+                  headers: {
+                      'Content-Type' : 'form-data'
+                  }
+              })
+      .then(response => (this.results = response.data))
+
+    }
+
+
+  }
 }
 </script>
 
