ea9de695f667ef0863702b80958a1b727d7b9c0a
Aljaz V <aljaz.verlic@gmail.com>
2021-05-23 10:54:29 +0200

    cant touch this

--- app/views.py
+++ app/views.py
@@ -1,3 +1,5 @@
+from json.decoder import JSONDecodeError
+from django.conf import settings
 from django.http.response import JsonResponse
 from app.parser import format_grouped_messages, group_by_day, group_by_day_list, parse_all, parse_messenger
 from django.shortcuts import render
@@ -16,55 +18,67 @@ def events(request, is_testing=False):
     date_from = request.GET.get('dateFrom',None)
     date_to = request.GET.get('dateTo',None)
 
-
-
-    # events = [
-    #     {
-    #         "date": "2020-07-08",
-    #         "content": "Hi filip"
-    #     }
-    # ]
-    messages = parse_messenger()
-    grouped_list = group_by_day_list(messages)
-
-     # Object of automatic summarization.
-    auto_abstractor = AutoAbstractor()
-    # Set tokenizer.
-    auto_abstractor.tokenizable_doc = SimpleTokenizer()
-    # Set delimiter for making a list of sentence.
-    auto_abstractor.delimiter_list = [".", "\n"]
-    # Object of abstracting and filtering document.
-    abstractable_doc = TopNRankAbstractor()
-    # Summarize document.
-
-    summaries = []
-
-    
-    if not is_testing:
-        if date_from and date_to:
-            date_from = datetime.strptime(date_from, '%Y-%m-%d')
-            date_to = datetime.strptime(date_to, '%Y-%m-%d')
-            grouped_list = list(filter(lambda x: date_from <= x["date"] <= date_to, grouped_list))
-
-        grouped_list = format_grouped_messages(grouped_list, "%b %d %Y")
-
-        for daily_message in grouped_list:
-            result_dict = auto_abstractor.summarize(daily_message["content"], abstractable_doc)
-            
-            daily_summary = "".join(result_dict["summarize_result"])
-            summaries.append(daily_summary)
-
-        return JsonResponse(summaries, safe=False)
+    if not settings.DEBUG:
+        summaries = [
+            {
+                "date": "18. June, 2020",
+                "content": "I really liked the party Andy organised yesterday. It was a blast!"
+            },
+            {
+                "date": "19. June, 2020",
+                "content": "Have you started to study for the exam next week yet? I heard its one of the most difficult exams this year."
+            },
+            {
+                "date": "20. June, 2020",
+                "content": "Hey have you heard from Ana, i heard she broke her arm last week while skating. Hope shes doing okay"
+            },
+            {
+                "date": "21. June, 2020",
+                "content": "Are you coming to the seminar tomorrow morning? I heard that attendance is mandatory and if you don't come you lose the right to attend exam."
+            }
+        ]
     else:
-        for daily_message in grouped_list:
-            result_dict = auto_abstractor.summarize(daily_message["content"], abstractable_doc)
-            
-            daily_summary = "".join(result_dict["summarize_result"])
-            summaries.append(daily_summary)
-    
-        return render(request, 'example.html' ,{
-            "data": zip(grouped_list, summaries)
-        })
+        messages = parse_messenger()
+        grouped_list = group_by_day_list(messages)
+
+        # Object of automatic summarization.
+        auto_abstractor = AutoAbstractor()
+        # Set tokenizer.
+        auto_abstractor.tokenizable_doc = SimpleTokenizer()
+        # Set delimiter for making a list of sentence.
+        auto_abstractor.delimiter_list = [".", "\n"]
+        # Object of abstracting and filtering document.
+        abstractable_doc = TopNRankAbstractor()
+        # Summarize document.
+
+        summaries = []
+
+        
+        if not is_testing:
+            if date_from and date_to:
+                date_from = datetime.strptime(date_from, '%Y-%m-%d')
+                date_to = datetime.strptime(date_to, '%Y-%m-%d')
+                grouped_list = list(filter(lambda x: date_from <= x["date"] <= date_to, grouped_list))
+
+            grouped_list = format_grouped_messages(grouped_list, "%b %d %Y")
+
+            for daily_message in grouped_list:
+                result_dict = auto_abstractor.summarize(daily_message["content"], abstractable_doc)
+                
+                daily_summary = "".join(result_dict["summarize_result"])
+                summaries.append(daily_summary)
+
+
+    return JsonResponse(summaries, safe=False)
+    # for daily_message in grouped_list:
+    #     result_dict = auto_abstractor.summarize(daily_message["content"], abstractable_doc)
+        
+    #     daily_summary = "".join(result_dict["summarize_result"])
+    #     summaries.append(daily_summary)
+
+    # return render(request, 'example.html' ,{
+    #     "data": zip(grouped_list, summaries)
+    # })
 
 def graph_points(request):
 
